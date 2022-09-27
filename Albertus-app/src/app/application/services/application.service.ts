09822bfa81a3605254e239dd0bfac514dcd3e61b
sebastian-trx <torressebastian014@gmail.com>
2022-09-27 12:49:01 -0500

    servicios de la aplicacion y el ws

--- Albertus-app/src/app/application/services/application.service.ts
+++ Albertus-app/src/app/application/services/application.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApplicationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createBlockchain(body:any){
+    return this.http.post(`${environment.urlBase}/create/blockchain`,body)
+  }
+  
+  createBlock(body:any){
+    return this.http.post(`${environment.urlBase}/create/block`,body)
+  }
+  
+  updateApp(body:any){
+    return this.http.put(`${environment.urlBase}/update/application`,body)
+  }
+  
+  deleteApp(body:any){
+    return this.http.put(`${environment.urlBase}/delete`,body)
+  }
+  
+  registerApp(body:any){
+    return this.http.post(`${environment.urlBase}/register/application`,body)
+  }
+
+  getBlockByHash(hash:string){
+    return this.http.get(`${environment.urlBaseView}/block/${hash}`);
+  }
+ 
+  getAllBlocksByApplicationId(idApplication:string){
+    return this.http.get(`${environment.urlBaseView}/blocks/${idApplication}`);
+  }
+  
+  getAllApplicationsByUserId(userId:string){
+    return this.http.get(`${environment.urlBaseView}/applications/${userId}`);
+  }
+
+
+
+
+  
+
 }
+
