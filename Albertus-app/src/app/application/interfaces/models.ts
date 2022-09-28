5eb33f6c60d45f419e1705b22334bdf709cd0b17
Santiago Sierra <santiagosierrak10@gmail.com>
2022-09-27 22:39:25 -0500

    service e interfaz

---
+++ Albertus-app/src/app/application/interfaces/models.ts
@@ -0,0 +1,13 @@
+// export type Block = {
+//     hash: string,
+//     timeStamp: string
+// }
+
+
+export type Block = {
+    timeStamp: string,
+    hash: string,
+    year: string,
+    month: string,
+    day: string;
+}
---
+++ Albertus-app/src/app/application/services/report.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportService {
+
+  constructor(
+      private http: HttpClient
+    ) { }
+
+
+
+  getAllBlocksByApplicationId(idApplication:string): Observable<any>{
+    return this.http.get(`${environment.urlBaseView}blocks/${idApplication}`);
+  }
+
+}
