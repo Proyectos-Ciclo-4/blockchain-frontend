638a15e2faa01e615f49adf2aebdfb87a9a1faf2
AndresTabor <andrestaborda43@gmail.com>
2022-09-27 20:38:35 -0500

    correcion de push en la main

--- Albertus-app/src/app/app-routing.module.ts
+++ Albertus-app/src/app/app-routing.module.ts
@@ -10,11 +10,6 @@ import {
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-    // TODO: proteccion de rutas
-  },
-  {
     path: 'my-apps',
     loadChildren: () => import('./application/application.module').then((m) => m.ApplicationModule),
     // TODO: proteccion de rutas
